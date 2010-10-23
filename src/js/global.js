de19fa4c13b74e4060340da6e6aefe6f8f8ed467
Mike Cravey <techie@craveytrain.com>
2010-10-22 21:10:48 -0500

    Fixing the layout of the time navs Adding dummy data

--- src/js/global.js
+++ src/js/global.js
@@ -29,3 +29,31 @@ var metro = {
 };
 
 document.addEventListener('DOMContentLoaded', metro.init, false);
+
+
+/* Temp Data */
+metro.data = {
+	southbound: {
+		Leander: ['5:25 AM', '6:00 AM', '6:35 AM', '7:10 AM', '7:54 AM'],
+		Lakeline: ['5:40 AM', '6:15 AM', '6:50 AM', '7:25 AM', '8:09 AM'],
+		Howard: ['5:53 AM', '6:28 AM', '7:03 AM', '7:38 AM', '8:02 AM', '8:22 AM'],
+		Kramer: ['6:00 AM', '6:35 AM', '7:10 AM', '7:45 AM', '8:08 AM', '8:29 AM'],
+		Crestvew: ['6:07 AM', '6:42 AM', '7:17 AM', '7:52 AM', '8:15 AM', '8:36 AM'],
+		Highland: ['6:10 AM', '6:45 AM', '7:20 AM', '7:55 AM', '8:18 AM', '8:39 AM'],
+		MLKJr: ['6:17 AM', '6:52 AM', '7:27 AM', '8:02 AM', '8:25 AM', '8:46 AM'],
+		PlazaSaltillo: ['6:23 AM', '6:58 AM', '7:33 AM', '8:08 AM', '8:31 AM', '8:52 AM'],
+		Downtown: ['6:27 AM', '7:02 AM', '7:37 AM', '8:12 AM', '8:35 AM', '8:56 AM']
+	},
+	northbound: {
+		Leander: ['7:43 AM'],
+		Lakeline: ['7:29 AM'],
+		Howard: ['7:16 AM', '7:50 AM'],
+		Kramer: ['7:07 AM', '7:44 AM'],
+		Crestvew: ['7:01 AM', '7:37 AM', '9:25 AM'],
+		Highland: ['6:58 AM', '7:34 AM', '9:22 AM'],
+		MLKJr: ['6:51 AM', '7:27 AM', '9:15 AM'],
+		PlazaSaltillo: ['6:43 AM', '7:19 AM', '9:07 AM'],
+		Downtown: ['6:41 AM', '7:17 AM', '9:05 AM']
+		
+	}
+};
