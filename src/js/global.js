0fbe365f6c79cc8475df2ee2feb0d70628ad3cdb
Mike Cravey <techie@craveytrain.com>
2010-10-22 23:33:00 -0500

    Adding stub methods to go fetch station and schedule Adding state objects for persistence Stubbing current time for working data

--- src/js/global.js
+++ src/js/global.js
@@ -1,21 +1,69 @@
 var metro = {
-	init: function () {
-		var nextNB = '5:25 AM',
-				nextSB = '6:05 AM',
-				refreshNB = setInterval(metro.ttl, 60000, nextNB, 'north'),
-				refreshSB = setInterval(metro.ttl, 60000, nextSB, 'south');
+	station: '',
+	north: {
+		schedule: [],
+		next: '',
+		ttl: ''
 	},
-	ttl: function (t, dir) {
-		var ttl = metro.delta(t);
-		if (ttl >= 0) {
-			var relMsg = document.querySelector('#' + dir + ' .relative');
-			relMsg.innerHTML = metro.relativeTS(ttl);
+	south: {
+		schedule: [],
+		next: '',
+		ttl: ''
+	},
+	init: function () {
+		// First things first, cache the station
+		metro.station = metro.getStation();
+	
+		// Find all the directions
+		var aRoutes = document.querySelectorAll('.route'),
+				l = aRoutes.length;
+	
+		while (l--) {
+			// Find the direction, get the schedule, find the next ride and show it
+			var dir = aRoutes[l].id;
+			metro[dir].schedule = metro.getSchedule(dir, metro.station);
+			metro.next.find(dir);
+			metro.next.show(dir);
+		}
+	
+/*
+		var refreshNB = setInterval(metro.ttl, 60000, nextNB, 'north'),
+				refreshSB = setInterval(metro.ttl, 60000, nextSB, 'south');
+*/				
+	},
+	next: {
+		find: function () {
+			var memo = 0;
+
+			var next = function (dir) {
+				metro[dir].ttl = metro.compareTime(metro[dir].schedule[memo]);
+
+				if (metro[dir].ttl < 0) {
+					memo = memo + 1;
+					next(dir);
+				}
+				metro[dir].time = metro[dir].schedule[memo];
+				return;
+			};
+			return next;
+		}(),
+		show: function (dir) {
+			var route = document.querySelector('#' + dir),
+					time = route.querySelector('time'),
+					relativeMsg = route.querySelector('.relative');
+			
+			time.innerHTML = metro[dir].time;
+			relativeMsg.innerHTML = metro.relativeTS(metro[dir].ttl);
+			
 		}
 	},
-	delta: function (t) {
+	compareTime: function (t) {
 		var now = new Date,
 				then = new Date;
-		
+				
+		// TODO: remove this line
+		now.setTime(Date.parse(then.toDateString() + ' ' + '6:20 AM'));
+				
 		if (t) then.setTime(Date.parse(then.toDateString() + ' ' + t));
 		
 		return Math.floor((then - now) / 60000);
@@ -25,6 +73,14 @@ var metro = {
 		if (d === 1) return 'in 1 minute';
 		if (d < 90) return 'in ' + d + ' minutes';
 		if (d < 1440) return 'in ' + Math.floor(d / 60) + ' hours';
+	},
+	getStation: function () {
+		// TODO: detect real station
+		return 'Lakeline';
+	},
+	getSchedule: function (dir, station) {
+		// TODO: get list from somewhere
+		return metro.data[dir][station];
 	}
 };
 
@@ -33,7 +89,7 @@ document.addEventListener('DOMContentLoaded', metro.init, false);
 
 /* Temp Data */
 metro.data = {
-	southbound: {
+	south: {
 		Leander: ['5:25 AM', '6:00 AM', '6:35 AM', '7:10 AM', '7:54 AM'],
 		Lakeline: ['5:40 AM', '6:15 AM', '6:50 AM', '7:25 AM', '8:09 AM'],
 		Howard: ['5:53 AM', '6:28 AM', '7:03 AM', '7:38 AM', '8:02 AM', '8:22 AM'],
@@ -44,7 +100,7 @@ metro.data = {
 		PlazaSaltillo: ['6:23 AM', '6:58 AM', '7:33 AM', '8:08 AM', '8:31 AM', '8:52 AM'],
 		Downtown: ['6:27 AM', '7:02 AM', '7:37 AM', '8:12 AM', '8:35 AM', '8:56 AM']
 	},
-	northbound: {
+	north: {
 		Leander: ['7:43 AM'],
 		Lakeline: ['7:29 AM'],
 		Howard: ['7:16 AM', '7:50 AM'],
