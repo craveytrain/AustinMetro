3d137668824045af02f935a12ff1f8e71f09eebb
Mike Cravey <techie@craveytrain.com>
2010-11-19 08:51:04 -0600

    Adding building out of next train times

--- src/js/view.js
+++ src/js/view.js
@@ -1,9 +1,11 @@
 metro.view = {
 	init: function () {
+		metro.util.sub('time', metro.view.time.init);
 		metro.util.sub('station', metro.view.station.set);
 		metro.util.sub('route', metro.view.station.list);
 		
 		if (metro.data.route.stations) metro.view.station.list();
+		metro.util.pub('route', [metro.data.route.name]);
 	},
 	station: {
 		set: function (station) {
@@ -36,6 +38,47 @@ metro.view = {
 		},
 		changeHandler: function () {
 			metro.util.pub('station', [this.value]);
+		}
+	},
+	time: {
+		init: function (dir, time, ttl) {
+			metro.view.time.build(dir, time, ttl);
+		},
+		build: function (dir, time, ttl) {
+			var parent = document.getElementById('upcoming'),
+					// Create nodes
+					section = document.createElement('section'),
+					heading = document.createElement('h1'),
+					headingTxt = document.createTextNode(dir),
+					nextCont = document.createElement('p'),
+					next = document.createElement('time'),
+					nextTxt = document.createTextNode(time),
+					relative = document.createElement('span'),
+					relativeTxt = document.createTextNode('in ' + ttl + ' minutes'),
+					nav = document.createElement('nav'),
+					list = document.createElement('ol'),
+					listItem = document.createElement('li');
+					
+			// Set attributes & values
+			section.className = 'route';
+			section.id = dir;
+			relative.className = 'relative';
+			
+			// Put it all together
+			section.appendChild(heading);
+			heading.appendChild(headingTxt);
+			section.appendChild(nextCont);
+			nextCont.appendChild(next);
+			next.appendChild(nextTxt);
+			nextCont.appendChild(relative);
+			relative.appendChild(relativeTxt);
+			section.appendChild(nav);
+			nav.appendChild(list);
+			list.appendChild(listItem);
+			
+			// Drop it in the DOM
+			parent.appendChild(section);
+					
 		}
 	}
 };
