c66feba4f6eba4c39409ff66747bab5d4bb6764e
Mike Cravey <techie@craveytrain.com>
2013-01-22 08:49:21 -0600

    Geo detect in flux

--- public/javascripts/cache.js
+++ public/javascripts/cache.js
@@ -2,36 +2,26 @@ define(['lib/promise'], function(promise) {
 	'use strict';
 
 	return function(url) {
-
-		var clearCache = function() {
-			var version = AM.version;
-			while (version--) {
-				var key = url + '-' + version;
-				if (localStorage.getItem(key)) localStorage.removeItem(key);
-			}
-		};
-
 		// Establish promise
 		var p = new promise.Promise();
+		var version = AM.version;
 
 		// version it
-		var key = url + '-' + AM.version;
+		// var key = url + '-' + AM.version;
 
 		// check for cache, if there, resolve promise
-		var data = localStorage.getItem(key);
-		if (data) p.done(null, JSON.parse(data));
+		var data = localStorage.getItem(url);
+		if (data) {
+			data = JSON.parse(data);
+			if (data.version === version) p.done(null, data);
+		}
 
 		// otherwise do the xhr, store it, resolve promise
 		promise
 			.get(url, null, {'X-Requested-With': 'XMLHttpRequest'})
 			.then(function(error, result) {
-				localStorage.setItem(key, result);
+				localStorage.setItem(url, result);
 				p.done(null, JSON.parse(result));
-
-				// wait a bit and scrub the cache
-				setTimeout(function() {
-					clearCache();
-				}, 1000);
 			});
 
 		// retun promise
