384d074dc96f57437cb60b841bd54b1c852a0f36
Mike Cravey <techie@craveytrain.com>
2013-05-03 09:56:01 -0500

    Changes to the time table

--- assets/js/station.js
+++ assets/js/station.js
@@ -1,11 +1,23 @@
-define(function() {
+define(['pluralize'], function() {
 	'use strict';
 
 	var isStation = function(path) {
 		return (/\/stations\/\w+/).test(path);
 	};
 
-	var hasPassed = (function() {
+	var relateTime = function(elapsedTime) {
+		var seconds = elapsedTime / 1000;
+		var minutes = Math.round(seconds / 60);
+		var hours = Math.round(minutes / 60);
+		if (Math.abs(hours) > 1) return hours + ' ' + 'hour'.pluralize(hours);
+		if (Math.abs(minutes) > 1) return minutes + ' ' + 'minute'.pluralize(minutes);
+
+		return seconds + ' ' + 'second'.pluralize(seconds);
+
+
+	};
+
+	var timePassed = (function() {
 		var now = new Date().getTime();
 		var then = new Date();
 
@@ -14,17 +26,25 @@ define(function() {
 			then.setHours(parsedTime[0]);
 			then.setMinutes(parsedTime[1]);
 
-			return then.getTime() < now;
+			return then.getTime() - now;
 		};
 	}());
 
 	var build = function() {
-		var times = document.querySelectorAll('.times li');
+		Array.prototype.forEach.call(document.querySelectorAll('.times'), function(timeTable) {
+			var future = false;
 
-		Array.prototype.forEach.call(times, function(time) {
-			if (!hasPassed(time.textContent)) return;
+			Array.prototype.forEach.call(timeTable.querySelectorAll('li'), function(time) {
+				var elapsedTime = timePassed(time.textContent);
+				var relTime = document.createElement('span');
 
-			time.parentNode.appendChild(time);
+				if (elapsedTime > 0 && !future) {
+					future = true;
+					time.classList.add('current');
+					relTime.textContent = relateTime(elapsedTime);
+					time.appendChild(relTime);
+				}
+			});
 		});
 	};
 
